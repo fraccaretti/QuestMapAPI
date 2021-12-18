a62b6f82a39e2a6119f6c503020a7d158b46b238
Piotr Fraccaro <piotr.fraccaro@northmill.se>
2021-12-18 11:53:04 +0100

    Add CORSMiddleware configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,6 +17,15 @@ public func configure(_ app: Application) throws {
 
     app.migrations.add(CreateWaypoints())
 
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    // cors middleware should come before default error middleware using `at: .beginning`
+    app.middleware.use(cors, at: .beginning)
+
     // register routes
     try routes(app)
 }
